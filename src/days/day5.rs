dc97a28d6f3da37d8ab917640a8f6b30564f0ca4
Gateway3B <matthewedwardweisfeld@gmail.com>
2023-12-14 00:56:02 +0000

    Day 5

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -11,6 +11,21 @@ struct Range {
     length: i64,
 }
 
+#[derive(PartialEq, Debug)]
+enum SeedRangeStatus {
+    Completed(SeedRange),
+    Uncompleted(SeedRange),
+}
+
+impl SeedRangeStatus {
+    pub fn extract(self: Self) -> SeedRange {
+        match self {
+            SeedRangeStatus::Completed(seed_range) => seed_range,
+            SeedRangeStatus::Uncompleted(seed_range) => seed_range,
+        }
+    }
+}
+
 impl Range {
     pub fn map(self: &Self, input: i64) -> Option<i64> {
         let index = input - self.destination_start;
@@ -18,6 +33,61 @@ impl Range {
             return None;
         } else {
             return Some(self.source_start + index);
+        }
+    }
+
+    pub fn split_map(self: &Self, seed_range: SeedRange) -> Vec<SeedRangeStatus> {
+        let index_start = seed_range.start - self.source_start;
+        let start_overlap = index_start > 0 && index_start < self.length;
+
+        let index_end = seed_range.end - self.source_start;
+        let end_overlap = index_end > 0 && index_end < self.length;
+
+        let total_overlap = index_start < 0 && index_end > self.length;
+
+        if total_overlap {
+            return vec![
+                SeedRangeStatus::Uncompleted(SeedRange {
+                    start: self.source_start,
+                    end: self.source_start - 1,
+                }),
+                SeedRangeStatus::Completed(SeedRange {
+                    start: self.destination_start,
+                    end: self.destination_start + self.length - 1,
+                }),
+                SeedRangeStatus::Uncompleted(SeedRange {
+                    start: self.source_start + self.length,
+                    end: seed_range.end,
+                }),
+            ];
+        }
+
+        match (start_overlap, end_overlap) {
+            (true, true) => vec![SeedRangeStatus::Completed(SeedRange {
+                start: self.destination_start + index_start,
+                end: self.destination_start + index_end,
+            })],
+            (true, false) => vec![
+                SeedRangeStatus::Completed(SeedRange {
+                    start: self.destination_start + index_start,
+                    end: self.destination_start + self.length - 1,
+                }),
+                SeedRangeStatus::Uncompleted(SeedRange {
+                    start: self.source_start + self.length,
+                    end: seed_range.end,
+                }),
+            ],
+            (false, true) => vec![
+                SeedRangeStatus::Uncompleted(SeedRange {
+                    start: seed_range.start,
+                    end: self.source_start - 1,
+                }),
+                SeedRangeStatus::Completed(SeedRange {
+                    start: self.destination_start,
+                    end: self.destination_start + index_end,
+                }),
+            ],
+            (false, false) => vec![SeedRangeStatus::Uncompleted(seed_range)],
         }
     }
 }
@@ -34,10 +104,45 @@ impl Map {
             .find_map(|range| range.map(input))
             .unwrap_or(input)
     }
+
+    pub fn split_map(self: &Self, seed_range: SeedRange) -> Vec<SeedRange> {
+        self.ranges
+            .iter()
+            .fold(
+                vec![SeedRangeStatus::Uncompleted(seed_range)],
+                |seed_ranges, range| {
+                    let (uncompleted, mut completed): (Vec<SeedRangeStatus>, Vec<SeedRangeStatus>) =
+                        seed_ranges.into_iter().partition(|seed_range| {
+                            matches!(seed_range, SeedRangeStatus::Uncompleted(_))
+                        });
+
+                    let new_seed_ranges =
+                        uncompleted
+                            .into_iter()
+                            .fold(Vec::new(), |mut seed_ranges, seed_range| {
+                                seed_ranges.extend(range.split_map(seed_range.extract()));
+                                seed_ranges
+                            });
+
+                    completed.extend(new_seed_ranges);
+
+                    completed
+                },
+            )
+            .into_iter()
+            .map(|seed_range_status| seed_range_status.extract())
+            .collect()
+    }
 }
 
 struct Maps {
     maps: Vec<Map>,
+}
+
+#[derive(Debug, PartialEq)]
+struct SeedRange {
+    start: i64,
+    end: i64,
 }
 
 pub struct Day5 {}
@@ -158,13 +263,16 @@ impl Solvable for Day5 {
             .map(|number| number.parse().map_err(anyhow::Error::msg))
             .collect::<Result<Vec<i64>>>()?;
 
-        let seeds = seeds.chunks(2).fold(Vec::new(), |mut seeds, slice| {
-            seeds.extend((slice[0]..slice[0] + slice[1]).collect::<Vec<i64>>());
-            seeds
-        });
+        let seed_ranges = seeds
+            .chunks(2)
+            .map(|slice| SeedRange {
+                start: slice[0],
+                end: slice[0] + slice[1],
+            })
+            .collect::<Vec<SeedRange>>();
 
         if debug {
-            println!("{:?}", seeds);
+            println!("{:?}", &seed_ranges);
         }
 
         let mut curr_map = Map { ranges: Vec::new() };
@@ -187,13 +295,13 @@ impl Solvable for Day5 {
                 }
 
                 let mut range_iter = line.split_whitespace();
-                let source_start = range_iter
-                    .next()
-                    .context("No source start number.")?
-                    .parse()?;
                 let destination_start = range_iter
                     .next()
                     .context("No destination start number.")?
+                    .parse()?;
+                let source_start = range_iter
+                    .next()
+                    .context("No source start number.")?
                     .parse()?;
                 let length = range_iter.next().context("No length number.")?.parse()?;
 
@@ -211,30 +319,52 @@ impl Solvable for Day5 {
         }
 
         if debug {
-            println!("{:#?}", maps.maps);
+            println!("{:#?}", &maps.maps);
         }
 
-        let mapped_seeds = seeds
-            .iter()
-            .map(|seed| {
+        let mapped_seed_ranges = seed_ranges
+            .into_iter()
+            .map(|seed_range| {
                 if debug {
-                    println!("Seed Input: {}", seed);
+                    println!("Seed Range: {:?}", &seed_range);
                 }
-                maps.maps.iter().fold(*seed, |seed, map| {
-                    let mapped_seed = map.map(seed);
-                    if debug {
-                        println!("\t{}", mapped_seed);
-                    }
-                    mapped_seed
-                })
+                maps.maps
+                    .iter()
+                    .fold(vec![seed_range], |mapped_seed_ranges, map| {
+                        let new_mapped_seed_ranges = mapped_seed_ranges.into_iter().fold(
+                            Vec::new(),
+                            |mut new_ranges, range| {
+                                let mut split_ranges = map.split_map(range);
+                                new_ranges.append(&mut split_ranges);
+                                new_ranges
+                            },
+                        );
+                        if debug {
+                            println!("\t{:?}", new_mapped_seed_ranges);
+                        }
+                        new_mapped_seed_ranges
+                    })
             })
-            .collect::<Vec<i64>>();
+            .collect::<Vec<Vec<SeedRange>>>();
+
+        let mapped_seed_ranges =
+            mapped_seed_ranges
+                .into_iter()
+                .fold(Vec::new(), |mut new_seed_ranges, seed_ranges| {
+                    new_seed_ranges.extend(seed_ranges);
+                    new_seed_ranges
+                });
 
         if debug {
-            println!("{:?}", &mapped_seeds);
+            println!("{:?}", &mapped_seed_ranges);
         }
 
-        let min = *mapped_seeds.iter().min().context("Couldn't find min.")?;
+        let min = mapped_seed_ranges
+            .into_iter()
+            .map(|seed_range| seed_range.start)
+            .filter(|range_start| range_start > &0)
+            .min()
+            .context("Couldn't find min.")?;
 
         let min = u32::try_from(min)?;
         Ok(min)
